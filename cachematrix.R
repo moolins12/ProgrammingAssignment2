ae9f812f8c8c561660e1be8f5733e7ff3797a052
moolins12 <39169605+moolins12@users.noreply.github.com>
2018-05-23 14:32:51 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I have created two functions below that execute the matrix inverse operation, but stores the data within the cache
+## to improve computing performance should the operation need to be executed many times.
 
-## Write a short comment describing this function
+## The first function creates cached variables for executing the inverse within the makeCacheMatrix function. In this way
+## the data can be pulled from within the function environment rather than the global environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv_mat <- NULL
+      set <- function(y) {
+            x <<- y
+            inv_mat <<- NULL
+      }
+      get <- function() {
+            x
+      }
+      setinverse <- function(inverse) {
+            inv_mat <<- inverse
+      }
+      getinverse <- function() {
+            inv_mat
+      }
+      
+      list(set = set, get = get, setinverse = setinverse,
+                  getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second function makes use of the predefined functions within makeCacheMatrix to pull the data and execute the inverse
+## operation and spit out a result.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+      inv_mat <- x$getinverse()
+      if(!is.null(inv_mat)) {
+            message("Getting cached data...")
+            return(inv_mat)
+      }
+      data <- x$get()
+      inv_mat <- solve(data, ...)
+      x$setinverse(inv_mat)
+      inv_mat
 }
